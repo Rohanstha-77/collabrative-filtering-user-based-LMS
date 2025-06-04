60079206b38f0052e52f3ef9a4648690508227b1
Rohan Shrestha <nikeshshrestha405@gmail.com>
2025-06-04 12:37:03 +0545

    update baseurl

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://collabrative-filtering-user-based-lms.onrender.com/',
 
 })
 
