c7168652a955b849db4e3fac980390608b1c5ceb
Rohan Shrestha <nikeshshrestha405@gmail.com>
2025-06-04 12:29:43 +0545

    Fix: re-adding backend files after updating .gitignore

--- backend/route/student-route/courses.js
+++
@@ -1,9 +0,0 @@
-import e from "express"
-import { getAllStudentCourses, getCoursesDetails } from "../../controllers/student-controllers/courseControllers.js"
-
-const router = e.Router()
-
-router.get('/get', getAllStudentCourses)
-router.get('/get/details/:id/:userId', getCoursesDetails)
-
-export default router
